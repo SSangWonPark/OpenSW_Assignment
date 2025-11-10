838748f821e45b1641d8353611f8b9dad2accbea
SSangWon <won3143111@gmail.com>
2025-11-10 10:44:49 +0900

    add my advanced ops

---
+++ week09_git/src/mymod.c
@@ -0,0 +1,5 @@
+#include "myops.h"
+
+double mymod(int a, int b){
+    return a % b;
+}
---
+++ week09_git/src/mypow.c
@@ -0,0 +1,6 @@
+#include "myops.h"
+#include <math.h>
+
+double mypow(int a, int b){
+    return pow(a, b);
+}
